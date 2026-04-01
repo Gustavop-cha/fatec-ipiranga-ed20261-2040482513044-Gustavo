b939cc008337d12ffbc3927f212b7feca79acd45
Gustavo_cha <spgu.spgu@gmail.com>
2026-03-31 21:48:47 -0300

    Add files via upload

---
+++ ED-2061-Atividades-N1/ED-20261-Atividade-N1-3/hp12c_rpn.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct {
+    double X, Y, Z, T;
+} PilhaHP;
+
+void inicializar(PilhaHP *p) {
+    p->X = p->Y = p->Z = p->T = 0.0;
+}
+
+void exibirEstado(PilhaHP *p) {
+    printf("\n[T]: %8.2f", p->T);
+    printf("\n[Z]: %8.2f", p->Z);
+    printf("\n[Y]: %8.2f", p->Y);
+    printf("\n[X]: %8.2f (Display)\n", p->X);
+    printf("--------------------------\n");
+}
+
+void push(PilhaHP *p, double valor) {
+    p->T = p->Z;
+    p->Z = p->Y;
+    p->Y = p->X;
+    p->X = valor;
+}
+
+void operar(PilhaHP *p, char op) {
+    double resultado = 0;
+    
+    if (op == '+') resultado = p->Y + p->X;
+    else if (op == '-') resultado = p->Y - p->X;
+    else if (op == '*') resultado = p->Y * p->X;
+    else if (op == '/') {
+        if(p->X != 0) resultado = p->Y / p->X;
+        else { printf("\nErro com a divisao"); return; }
+    }
+
+    p->X = resultado;
+    p->Y = p->Z;
+    p->Z = p->T;
+    p->T = 0; 
+}
+
+int main() {
+    PilhaHP minhaPilha;
+    inicializar(&minhaPilha);
+
+    char expressao[100];
+    printf("Digite a expressao RPN (ex: 5 1 2 + 4 * + 3 -): ");
+    fgets(expressao, 100, stdin);
+
+    char *token = strtok(expressao, " \n");
+
+    while (token != NULL) {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
+            push(&minhaPilha, atof(token));
+        } 
+        else if (strchr("+-*/", token[0])) {
+            operar(&minhaPilha, token[0]);
+        }
+
+        exibirEstado(&minhaPilha);
+        token = strtok(NULL, " \n");
+    }
+
+    printf("\nO resultado da expressao algebrica e: %.2f\n", minhaPilha.X);
+    return 0;
+}
